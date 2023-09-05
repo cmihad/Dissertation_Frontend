7f65f90df044bc3ca835670917d59bd1e7721a8a
Mihad choudhury <cmihad@gmail.com>
2023-09-05 18:57:39 +0100

    updated the landing page

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import TheFooter from './components/Navigation/TheFooter.vue'
 
 <template>
   <TheHeader />
-  <div class="max-w-screen-xl mx-auto py-4">
+  <div class="max-w-screen-xl mx-auto">
     <div class="container mx-auto">
       <RouterView />
     </div>
